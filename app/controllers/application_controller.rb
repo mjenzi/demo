0998e6e138a7b3c2283e1ba965539068efa75003
zeeshan-za-ahmad <zeeshan.za.ahmad@gmail.com>
2017-01-23 23:09:14 +0500

    Adds Services

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,27 @@ class ApplicationController < ActionController::Base
   def index
   	render "index.html"
   end
+  def webDesign
+  	@service = getService('Web Design')
+  	render "services.html"
+  end
+
+  def internet
+  	@service = getService('Internet of Things')
+  	render "services.html"
+  end
+
+  def digital
+  	@service = getService('Digital Strategy Consulting ')
+  	render "services.html"
+  end
+
+  def bigData
+  	@service = getService('Big Data Analytics.')
+  	render "services.html"
+  end
+
+  def getService(name)
+  	return {image: "02.png",name: name,tagline:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia sint rem inventore est reprehenderit voluptatum, placeat delectus dolore quam ab ea, illum omnis! Laboriosam neque accusantium ex aliquam, reiciendis, quo."}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "application#index"
+  get 'web-application-development' => 'application#webDesign', as: :web_design
+  get 'internet-of-things' => 'application#internet', as: :internet
+  get 'digital-strategy-consulting' => 'application#digital', as: :digital
+  get 'big-data-analytics' => 'application#bigData', as: :big_data
+
 end
